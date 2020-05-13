68d524dc033727c6beb426a82a8225da71efad7f
Carlos Galo <cgalo13@gmail.com>
2020-05-13 09:14:07 -0400

    Created new header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,20 @@ import { NgModule }                 from '@angular/core';
 import { FormsModule }              from '@angular/forms';
 import { MatInputModule }           from '@angular/material/input';
 import { MatCardModule }            from '@angular/material/card';
-import { MatButtonModule }            from '@angular/material/button';
-
+import { MatButtonModule }          from '@angular/material/button';
+import { MatToolbarModule }         from '@angular/material/toolbar';
 import { AppRoutingModule }         from './app-routing.module';
+
 import { AppComponent }             from './app.component';
 import { PostCreateComponent }      from './posts/post-create/post-create.component';
+import { HeaderComponent }          from './header/header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +26,8 @@ import { PostCreateComponent }      from './posts/post-create/post-create.compon
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
